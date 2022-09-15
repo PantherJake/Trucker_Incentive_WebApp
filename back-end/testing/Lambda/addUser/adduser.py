f1b9d8dd2ed29b4775fb0679192263cb21075cf1
mhilgen <max.hilgenberg@gmx.net>
2022-09-15 17:28:39 -0400

    added lambda function and research file

---
+++ back-end/testing/Lambda/addUser/adduser.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+import logging
+import rds_config
+import pymysql
+import userinfo
+
+
+
+rds_host  = rds_config.db_endpoint
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+port = 3306
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(host=rds_host, user=name,passwd=password, db=db_name)
+
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
+def sql(event, context):
+    
+    with conn.cursor() as cur:
+        #insert specific data into the user table which is grabbed from a userinfo.py file at the moment
+        query = 'insert into testusers(user_fname, user_lname, user_org_id) values("'+ str(userinfo.userfname) + '","'+str(userinfo.userlname)+'",'+str(userinfo.user_org_id)+')'
+        cur.execute(query)
+        print(query)
+        conn.commit()
+        
+        test = cur.execute("select * from testusers")
+        print(test)
+        item_count = 0
+        for row in cur:
+            item_count += 1
+            logger.info(row)
+       
+    
+    return event
---
+++ back-end/testing/Lambda/addUser/userinfo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# data from user 
+userfname = "Max"
+userlname = "Hilgenberg"
+user_org_id = 78
+
