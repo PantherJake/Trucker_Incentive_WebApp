420d66f0c148755dac924411d46024d4c0b76936
dbakyle <47783021+dbakyle@users.noreply.github.com>
2022-09-17 10:39:27 -0400

    Updating getuser adding getorg

--- back-end/testing/Lambda/getorg/getorg.py
+++ back-end/testing/Lambda/getorg/getorg.py
@@ -5,7 +5,6 @@ import rds_config
 import pymysql
 import json
 from pprint import pprint
-import boto3
 
 """
 Team 8
@@ -28,25 +27,22 @@ except:
     sys.exit()
 
 logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
-
-client = boto3.client("lambda")
-def getusers(event, context):
+def getorg(event, context):
     """
-    This function will return the users given a provided filter
+    This function will return the org given a provided filter
     The filter should be and can be combined:
-        None = Return all users in the system
-        { "org_id" : "<value>"  Return users of an org
-        { "role_id" : "<value>  Return useres of a role
+        None = Return all orgs in the system
+        { "id" : "<value>"  Return users of an org
     """
 
-    querystr = "select user_id id, user_fname fname, user_mname mname, user_lname lname, user_role_id role, user_email email, user_org_id org_id " \
-                "from users "
+    querystr = "select org_id, org_name " \
+                "from organization "
     querywhere = ""
     param = ""
 
     if 'id' in event:
         id = event['id']
-        querywhere = "where user_id = %s"
+        querywhere = "where org_id = %s"
         param = (id)
     else:
         id = ""
@@ -66,32 +62,14 @@ def getusers(event, context):
         names = cur.description
         cnt = 0
         for row in cur:
-            logger.info(row) 
             for col in row:
-                if names[cnt][0] == "org_id":
-                    orgdet = getorgfromFn({'id':str(col)})
-                    retval = retval + '"org":' + orgdet + ','
-                else:
-                    retval = retval + '"' + names[cnt][0] + '":"' + str(col) + '"' + ','
+                retval = retval + '"' + names[cnt][0] + '":"' + str(col) + '"' + ','
+                logger.info(row)    
                 cnt = cnt + 1
+                
     # if the last value is a , remove it.
     if retval[len(retval)-1] == ',':
-        retval = retval[0:len(retval)-1]
-        
-    retval = '{' + '"user_count":' + str(rowcnt) + ',' + retval + "}"
+        retval = retval[0:len(retval)-2]
+    retval = '{' + '"org_count":' + str(rowcnt) + ',' + retval + "}"
     #conn.close()
     return retval
-
-def getorgfromFn(inputParams):
-        #inputParams = { 'id' : '1'}
-    
-    org = client.invoke(
-        FunctionName = "arn:aws:lambda:us-east-1:274815321855:function:team8getorg",
-        InvocationType = "RequestResponse",
-        Payload = json.dumps(inputParams)
-    )
-    
-    responsefromorg = json.load(org['Payload'])
-    print("\n")
-    print(responsefromorg)
-    return responsefromorg
