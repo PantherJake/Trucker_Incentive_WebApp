6c46f8b29d20aaf447109e1e9eadbb81c952084d
Panther Jake <jvickio@clemson.edu>
2022-09-23 15:53:52 -0400

    fixed api issue

--- front-end/truck-incentive-program/src/components/LogIn.vue
+++ front-end/truck-incentive-program/src/components/LogIn.vue
@@ -6,14 +6,14 @@
   </head>    
     <body>    
       <h1 style="align-self: center;"> Driver Incentive Login </h1>  
-        <form>
+        <form method="GET" action="https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/users/7">
           <div class="container">   
             <label>Email : </label>   
             <input type="text" v-model="Email" placeholder="Enter Email" name="Email" required>  
             <label>Password : </label>   
             <input type="password" v-model="Password" placeholder="Enter Password" name="Password" required>  
             
-            <button type="submit" @click="validateLogin">Login</button>   
+            <button type="submit">Login</button>
             
             <input type="checkbox" checked="checked">Remember me   
             <button type="button" class="cancelbtn">Cancel</button>   
@@ -25,44 +25,17 @@
 </template> 
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 
 export default {
   name: 'LoginPage',
-  data: function() {
+  data() {
     return {
       Email: '',
       Password: '',
       isRemember: false,
-      data: [],
-      errors: []
     }
   },
-  methods: {
-    async validateLogin() {
-      try {
-        console.log(this.Email);
-        console.log(this.Password);
-
-        const res = await axios.get('https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/users/7');
-        this.data = res.data
-        console.log(this.data);
-      } catch(e) {
-        this.errors.push(e);
-      }
-    },
-  }
-  // mounted() {
-  //   axios
-  //     .get('https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/users/7')
-  //     .then(response => {
-  //       this.data = response.data
-  //     })
-  //     .catch(error => {
-  //       console.log(error)
-  //       this.errored = true
-  //     })
-  // }
 }
 </script>
 
