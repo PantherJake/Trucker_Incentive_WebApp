54028e36864933d28daaafed1e9f1ede77918177
Julia Blahut <jblahut@clemson.edu>
2022-10-03 15:03:52 +0000

    Modified index.js to have driverdashboard in router

--- front-end/truck-incentive-program/src/router/index.js
+++ front-end/truck-incentive-program/src/router/index.js
@@ -4,7 +4,8 @@ import {createRouter, createWebHistory} from "vue-router";
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/login', name:'LogInPage', component: ()=>import('@/views/LogInPage')},
-    {path: '/apply', name:'ApplicationPage', component: ()=>import('@/views/ApplicationPage')}
+    {path: '/apply', name:'ApplicationPage', component: ()=>import('@/views/ApplicationPage')},
+    {path: '/driverdashboard', name:'DriverDashboardPage', component: ()=>import('@/views/DriverDashboardPage')}
 ]
 
 const router = createRouter({
