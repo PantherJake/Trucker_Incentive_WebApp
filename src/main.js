03f61688b48455a3a7a998914de6cde88311d623
Panther Jake <jvickio@clemson.edu>
2022-10-11 23:08:24 -0400

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 import App from './App.vue'
 import router from './router'
 
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
-import awsmobile from "@/aws-exports";
+import awsmobile from "./aws-exports";
 
 Amplify.configure(awsmobile)
 createApp(App).use(router).use(AmplifyPlugin, AmplifyModules).mount('#app')
