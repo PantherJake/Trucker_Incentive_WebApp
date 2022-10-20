15723173bb5e4ac6694fe1a178e333f22f2c20e7
Panther Jake <jvickio@clemson.edu>
2022-10-20 17:23:58 -0400

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,14 @@
 import Home from '@/views/HomePage'
 import {createRouter, createWebHistory} from "vue-router";
+import {Auth} from "aws-amplify";
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-    {path: '/login',
-        name:'LogInPage',
-        query: {redirects: 'driverdashboard'},
-        component: ()=>import('@/views/LogInPage')},
+    {path: '/login', name:'LogInPage', component: ()=>import('@/views/LogInPage')},
     {path: '/apply', name:'ApplicationPage', component: ()=>import('@/views/ApplicationPage')},
-    {path: '/profile', name:'ProfilePage', component: ()=>import('@/views/ProfilePage')},
-    {path: '/cart', name:'CartPage', component: ()=>import('@/views/CartPage')},
-    {path: '/driverdashboard', name:'DriverDashboardPage', component: ()=>import('@/views/DriverDashboardPage')},
+    {path: '/profile', name:'ProfilePage', component: ()=>import('@/views/ProfilePage'), meta: {requiresAuth: true}},
+    {path: '/cart', name:'CartPage', component: ()=>import('@/views/CartPage'), meta: {requiresAuth: true}},
+    {path: '/driverdashboard', name:'DriverDashboardPage', component: ()=>import('@/views/DriverDashboardPage'), meta: {requiresAuth: true}},
     {path: '/sponsordashboard', name:'SponsorDashboardPage', component: ()=>import('@/views/SponsorDashboardPage')},
     {path: '/admindashboard', name:'AdminDashboardPage', component: ()=>import('@/views/AdminDashboardPage')},
     {path: '/driverdashboard/points', name:'DriverPointsPage', component: ()=>import('@/components/driver/DriverPoints')},
@@ -26,5 +24,22 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeResolve(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        try {
+            await Auth.currentAuthenticatedUser();
+            next();
+        } catch (e) {
+            next({
+                path: "/",
+                query: {
+                    redirect: to.fullPath
+                }
+            });
+        }
+    }
+    next();
+});
 
 export default router
