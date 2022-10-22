f2d99a5e4e8dcb644209de0cc0acea8059087f6f
Panther Jake <jvickio@clemson.edu>
2022-10-22 16:32:39 -0400

    403 error

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
+  <html>
+  <head>
+    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+    <title>Trucker Incentive App</title>
+  </head>
   <div>
     <router-view />
   </div>
+  </html>
 </template>
 
 <script>
 export default {
   name: 'App'
-  
+
 }
 </script>
 
