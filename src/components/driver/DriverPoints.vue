83b28a87b79a24aa3316a862b6c40dc3c93ee0a7
Panther Jake <jvickio@clemson.edu>
2022-10-24 01:11:06 -0400

    api fix:

--- src/components/driver/DriverPoints.vue
+++ src/components/driver/DriverPoints.vue
@@ -55,7 +55,7 @@ export default {
   },
   created() {
     try {
-      const response = fetch("https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/orgs/" + this.orgID + "/drivers/" + this.driverID + "/topdrivers?orgid=" + this.orgID, {
+      const response = fetch("https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/orgs/" + this.orgID + "/drivers/" + this.driverID + "/topdrivers?orgid=" + this.orgID + "&limit=10", {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
