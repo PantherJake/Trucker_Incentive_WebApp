cffc5f790735b28d060b1e9faf24fc325d4cd975
Panther Jake <jvickio@clemson.edu>
2022-11-03 18:16:59 -0400

    userid query

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -92,7 +92,7 @@ export default {
         this.user = JSON.parse(this.userObj)
 
         try {
-          this.dbObj = await fetch("https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/me?userid=1", {
+          this.dbObj = await fetch("https://niiertdkbf.execute-api.us-east-1.amazonaws.com/prod/users?userid=1", {
             method: 'GET', // *GET, POST, PUT, DELETE, etc.
             mode: 'cors', // no-cors, *cors, same-origin
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
